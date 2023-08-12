6feff69e61be9c21cca9d71af9c116607513e410
Paul Riseborough <gncsolns@gmail.com>
2023-08-12 17:57:11 +1000

    AP_AHRS: Replace separate NE positions with a Vector2f

--- libraries/AP_AHRS/AP_AHRS_DCM.cpp
+++ libraries/AP_AHRS/AP_AHRS_DCM.cpp
@@ -770,16 +770,14 @@ AP_AHRS_DCM::drift_correction(float deltat)
         _last_lat = _gps.location().lat;
         _last_lng = _gps.location().lng;
         _last_pos_ms = AP_HAL::millis();
-        _position_offset_north = 0;
-        _position_offset_east = 0;
+        _position_offset_NE.zero();
 
         // once we have a single GPS lock, we can update using
         // dead-reckoning from then on
         _have_position = true;
     } else {
         // update dead-reckoning position estimate
-        _position_offset_north += velocity.x * _ra_deltat;
-        _position_offset_east  += velocity.y * _ra_deltat;
+        _position_offset_NE += velocity.xy() * _ra_deltat;
     }
 
     // see if this is our first time through - in which case we
@@ -1072,7 +1070,7 @@ bool AP_AHRS_DCM::get_location(Location &loc) const
         alt_cm = baro.get_altitude() * 100 + AP::ahrs().get_home().alt;
     }
     loc.set_alt_cm(alt_cm, Location::AltFrame::ABSOLUTE);
-    loc.offset(_position_offset_north, _position_offset_east);
+    loc.offset(_position_offset_NE.x, _position_offset_NE.y);
     if (_have_position) {
         const uint32_t now = AP_HAL::millis();
         float dt = 0;
