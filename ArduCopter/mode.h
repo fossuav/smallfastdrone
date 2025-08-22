ArduPilot/ardupilot

Copter: add new Rate Acro mode
scale I to angle P for rate-only acro

ArduCopter/mode.h
@@ -101,6 +101,7 @@ public:
         AUTOROTATE =   26,  // Autonomous autorotation
         AUTO_RTL =     27,  // Auto RTL, this is not a true mode, AUTO will report as this mode if entered to perform a DO_LAND_START Landing sequence
         TURTLE =       28,  // Flip over after crash
+        RATE_ACRO =    29,  // Betaflight-style rate acro
 
         // Mode number 30 reserved for "offboard" for external/lua control.
 
@@ -442,11 +443,32 @@ protected:
     // get_pilot_desired_rates_rads - transform pilot's normalised roll pitch and yaw input into a desired lean angle rates
     // inputs are -1 to 1 and the function returns desired angle rates in centi-degrees-per-second
     void get_pilot_desired_rates_rads(float roll_in_norm, float pitch_in_norm, float yaw_in_norm, float &roll_out_rads, float &pitch_out_rads, float &yaw_out_rads);
+    void get_desired_rates(float& target_roll_rads, float& target_pitch_rads, float& target_yaw_rads, float& pilot_desired_throttle);
 
     float throttle_hover() const override;
 
-private:
     bool disable_air_mode_reset;
+};
+#endif
+
+#if MODE_RATE_ACRO_ENABLED
+class ModeRateAcro : public ModeAcro {
+
+public:
+    // inherit constructor
+    using ModeAcro::ModeAcro;
+    Number mode_number() const override { return Number::RATE_ACRO; }
+
+    virtual void run() override;
+    bool init(bool ignore_checks) override;
+    void exit() override;
+    // Called when air mode is enabled via AUX switch; prevents automatic reset to default air_mode state
+    void air_mode_aux_changed();
+
+protected:
+
+    const char *name() const override { return "ACRR"; }
+    const char *name4() const override { return "RATEACRO"; }
 };
 #endif
 

ArduCopter/mode_acro.cpp
@@ -7,10 +7,9 @@
 /*
  * Init and run calls for acro flight mode
  */
-void ModeAcro::run()
+void ModeAcro::get_desired_rates(float& target_roll_rads, float& target_pitch_rads, float& target_yaw_rads, float& pilot_desired_throttle)
 {
     // convert the input to the desired body frame rate
-    float target_roll_rads, target_pitch_rads, target_yaw_rads;
     get_pilot_desired_rates_rads(channel_roll->norm_input_dz(), channel_pitch->norm_input_dz(), channel_yaw->norm_input_dz(), target_roll_rads, target_pitch_rads, target_yaw_rads);
 
     if (!motors->armed()) {
@@ -28,7 +27,7 @@ void ModeAcro::run()
         motors->set_desired_spool_state(AP_Motors::DesiredSpoolState::THROTTLE_UNLIMITED);
     }
 
-    float pilot_desired_throttle = get_pilot_desired_throttle();
+    pilot_desired_throttle = get_pilot_desired_throttle();
 
     switch (motors->get_spool_state()) {
     case AP_Motors::SpoolState::SHUT_DOWN:
@@ -57,6 +56,12 @@ void ModeAcro::run()
         // do nothing
         break;
     }
+}
+
+void ModeAcro::run()
+{
+    float target_roll_rads, target_pitch_rads, target_yaw_rads, pilot_desired_throttle;
+    get_desired_rates(target_roll_rads, target_pitch_rads, target_yaw_rads, pilot_desired_throttle);
 
     // run attitude controller
     if (g2.acro_options.get() & uint8_t(AcroOptions::RATE_LOOP_ONLY)) {
@@ -199,3 +204,40 @@ void ModeAcro::get_pilot_desired_rates_rads(float roll_in_norm, float pitch_in_n
     yaw_out_rads = rate_bf_request_rads.z;
 }
 #endif
+
+#if MODE_RATE_ACRO_ENABLED
+/*
+ * Init and run calls for rate acro flight mode
+ */
+void ModeRateAcro::run()
+{
+    float target_roll_rads, target_pitch_rads, target_yaw_rads, pilot_desired_throttle;
+
+    get_desired_rates(target_roll_rads, target_pitch_rads, target_yaw_rads, pilot_desired_throttle);
+    // send rate commands to attitude controller (RATE_LOOP_ONLY bypasses full attitude stabilization)
+    attitude_control->input_rate_bf_roll_pitch_yaw_2_cds(target_roll_rads, target_pitch_rads, target_yaw_rads);
+
+    // output pilot's throttle without angle boost
+    attitude_control->set_throttle_out(pilot_desired_throttle, false, copter.g.throttle_filt);
+}
+
+bool ModeRateAcro::init(bool ignore_checks)
+{
+    disable_air_mode_reset = false;
+    copter.air_mode = AirMode::AIRMODE_ENABLED;
+    attitude_control->convert_angle_P_to_I(true);
+
+    return true;
+}
+
+void ModeRateAcro::exit()
+{
+    if (!disable_air_mode_reset) {
+        copter.air_mode = AirMode::AIRMODE_DISABLED;
+    }
+    attitude_control->convert_angle_P_to_I(false);
+    disable_air_mode_reset = false;
+}
+
+#endif // MODE_RATE_ACRO_ENABLED
+

fossuav/smallfastdrone

ArduCopter/mode.h
@@ -101,6 +101,7 @@ public:
         AUTOROTATE =   26,  // Autonomous autorotation
         AUTO_RTL =     27,  // Auto RTL, this is not a true mode, AUTO will report as this mode if entered to perform a DO_LAND_START Landing sequence
         TURTLE =       28,  // Flip over after crash
+        RATE_ACRO =    29,  // Betaflight-style rate acro
 
         // Mode number 127 reserved for the "drone show mode" in the Skybrush
         // fork at https://github.com/skybrush-io/ardupilot
@@ -440,11 +441,32 @@ protected:
     // get_pilot_desired_angle_rates - transform pilot's normalised roll pitch and yaw input into a desired lean angle rates
     // inputs are -1 to 1 and the function returns desired angle rates in centi-degrees-per-second
     void get_pilot_desired_angle_rates(float roll_in, float pitch_in, float yaw_in, float &roll_out, float &pitch_out, float &yaw_out);
+    void get_desired_rates(float& target_roll, float& target_pitch, float& target_yaw, float& pilot_desired_throttle);
 
     float throttle_hover() const override;
 
-private:
     bool disable_air_mode_reset;
+};
+#endif
+
+#if MODE_RATE_ACRO_ENABLED
+class ModeRateAcro : public ModeAcro {
+
+public:
+    // inherit constructor
+    using ModeAcro::ModeAcro;
+    Number mode_number() const override { return Number::RATE_ACRO; }
+
+    virtual void run() override;
+    bool init(bool ignore_checks) override;
+    void exit() override;
+    // Called when air mode is enabled via AUX switch; prevents automatic reset to default air_mode state
+    void air_mode_aux_changed();
+
+protected:
+
+    const char *name() const override { return "ACRR"; }
+    const char *name4() const override { return "RATEACRO"; }
 };
 #endif
 

ArduCopter/mode_acro.cpp
@@ -7,10 +7,9 @@
 /*
  * Init and run calls for acro flight mode
  */
-void ModeAcro::run()
+void ModeAcro::get_desired_rates(float& target_roll, float& target_pitch, float& target_yaw, float& pilot_desired_throttle)
 {
     // convert the input to the desired body frame rate
-    float target_roll, target_pitch, target_yaw;
     get_pilot_desired_angle_rates(channel_roll->norm_input_dz(), channel_pitch->norm_input_dz(), channel_yaw->norm_input_dz(), target_roll, target_pitch, target_yaw);
 
     if (!motors->armed()) {
@@ -28,7 +27,7 @@ void ModeAcro::run()
         motors->set_desired_spool_state(AP_Motors::DesiredSpoolState::THROTTLE_UNLIMITED);
     }
 
-    float pilot_desired_throttle = get_pilot_desired_throttle();
+    pilot_desired_throttle = get_pilot_desired_throttle();
 
     switch (motors->get_spool_state()) {
     case AP_Motors::SpoolState::SHUT_DOWN:
@@ -57,6 +56,12 @@ void ModeAcro::run()
         // do nothing
         break;
     }
+}
+
+void ModeAcro::run()
+{
+    float target_roll, target_pitch, target_yaw, pilot_desired_throttle;
+    get_desired_rates(target_roll, target_pitch, target_yaw, pilot_desired_throttle);
 
     // run attitude controller
     if (g2.acro_options.get() & uint8_t(AcroOptions::RATE_LOOP_ONLY)) {
@@ -198,3 +203,40 @@ void ModeAcro::get_pilot_desired_angle_rates(float roll_in, float pitch_in, floa
     yaw_out = rate_bf_request_cd.z;
 }
 #endif
+
+#if MODE_RATE_ACRO_ENABLED
+/*
+ * Init and run calls for rate acro flight mode
+ */
+void ModeRateAcro::run()
+{
+    float target_roll, target_pitch, target_yaw, pilot_desired_throttle;
+
+    get_desired_rates(target_roll, target_pitch, target_yaw, pilot_desired_throttle);
+    // send rate commands to attitude controller (RATE_LOOP_ONLY bypasses full attitude stabilization)
+    attitude_control->input_rate_bf_roll_pitch_yaw_2(target_roll, target_pitch, target_yaw);
+
+    // output pilot's throttle without angle boost
+    attitude_control->set_throttle_out(pilot_desired_throttle, false, copter.g.throttle_filt);
+}
+
+bool ModeRateAcro::init(bool ignore_checks)
+{
+    disable_air_mode_reset = false;
+    copter.air_mode = AirMode::AIRMODE_ENABLED;
+    attitude_control->convert_angle_P_to_I(true);
+
+    return true;
+}
+
+void ModeRateAcro::exit()
+{
+    if (!disable_air_mode_reset) {
+        copter.air_mode = AirMode::AIRMODE_DISABLED;
+    }
+    attitude_control->convert_angle_P_to_I(false);
+    disable_air_mode_reset = false;
+}
+
+#endif // MODE_RATE_ACRO_ENABLED
+