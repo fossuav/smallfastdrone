e04ca5a71a6f5c3ea5f9e535f4c30d3f4ee2494c
Andy Piper <github@andypiper.com>
2026-02-13 17:27:29 +0000

    Copter: version update

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V4.7.0-beta8"
+#define THISFIRMWARE "SmallFastDrone V4.7.0"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,7,0,FIRMWARE_VERSION_TYPE_BETA+7
