ArduPilot/ardupilot

Copter: restore throw-drop baro de-weight during spool-up
After a throw drop is detected (land_complete cleared) the motors spool up and
prop wash contaminates the baro. Allow the ground-effect takeoff window through
the spool-up instead of vetoing it for all of THROW, so the EKF de-weights the
disturbed baro. The de-weight is suppressed before detection as before.

ArduCopter/baro_ground_effect.cpp
@@ -5,7 +5,11 @@ void Copter::update_ground_effect_detector(void)
 {
     AP_GroundEffect &gndeff = g2.ground_effect;
 
-    gndeff.set_takeoff_expected(flightmode->mode_number() != Mode::Number::THROW);
+    // THROW suppresses the takeoff baro-deweight until a drop is detected
+    // (land_complete is still set); after detection, motor prop wash
+    // contaminates the baro, so allow the deweight window through the spool-up.
+    const bool in_throw = flightmode->mode_number() == Mode::Number::THROW;
+    gndeff.set_takeoff_expected(!in_throw || !ap.land_complete);
     gndeff.set_high_vibrations(vibration_check.high_vibes);
 
     // ALT_HOLD has manual attitude and no NE controller, so a near-level

fossuav/smallfastdrone

ArduCopter/baro_ground_effect.cpp
@@ -5,8 +5,11 @@ void Copter::update_ground_effect_detector(void)
 {
     AP_GroundEffect &gndeff = g2.ground_effect;
 
-    // throw mode never wants the takeoff expected EKF code
-    gndeff.enable_takeoff_comp(flightmode->mode_number() != Mode::Number::THROW);
+    // THROW suppresses the takeoff baro-deweight until a drop is detected
+    // (land_complete is still set); after detection, motor prop wash
+    // contaminates the baro, so allow the deweight window through the spool-up.
+    const bool in_throw = flightmode->mode_number() == Mode::Number::THROW;
+    gndeff.enable_takeoff_comp(!in_throw || !ap.land_complete);
     gndeff.set_high_vibrations(vibration_check.high_vibes);
 
     // ALT_HOLD has manual attitude and no NE controller, so a near-level