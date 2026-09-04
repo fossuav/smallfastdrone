8322f45fc349d2f6b0f92e0f9b8d1a8996c17852
Andy Piper <github@andypiper.com>
2026-09-04 11:17:54 +0100

    fixup! AP_NavEKF3: suppress ResetHeight during baro ground effect

--- libraries/AP_NavEKF3/AP_NavEKF3_core.cpp
+++ libraries/AP_NavEKF3/AP_NavEKF3_core.cpp
@@ -272,6 +272,7 @@ void NavEKF3_core::InitialiseVariables()
     manoeuvring = false;
     fusingStationaryZeroVel = false;
     fusingGndEffectHgtRef = false;
+    gndEffectHgtResetSuppressStart_ms = 0;
     inhibitWindStates = true;
     windStateIsObservable = false;
     treatWindStatesAsTruth = false;
