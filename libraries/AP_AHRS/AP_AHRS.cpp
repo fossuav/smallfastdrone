9a6c6bd4443efcf8612451795b7bdc1ee7bb1d49
Andy Piper <github@andypiper.com>
2026-09-05 09:26:23 +0100

    AP_AHRS: use the 4.7 EKF3 accessor for the hover Z-bias calls
    #32471 is written against master's backends_and_estimates refactor, which
    4.7 does not have.

--- libraries/AP_AHRS/AP_AHRS.cpp
+++ libraries/AP_AHRS/AP_AHRS.cpp
@@ -3728,7 +3728,7 @@ const EKFGSF_yaw *AP_AHRS::get_yaw_estimator(void) const
 float AP_AHRS::get_hover_z_bias_correction(uint8_t imu_index) const
 {
 #if HAL_NAVEKF3_AVAILABLE
-    return ekf3.EKF3.hoverZBiasCorrection(imu_index);
+    return EKF3.hoverZBiasCorrection(imu_index);
 #else
     return 0.0f;
 #endif
@@ -3738,7 +3738,7 @@ bool AP_AHRS::get_accel_bias_z_for_imu(uint8_t imu_index, float &bias_z) const
 {
 #if HAL_NAVEKF3_AVAILABLE
     Vector3f accel_bias;
-    if (ekf3.EKF3.getAccelBiasForIMU(imu_index, accel_bias)) {
+    if (EKF3.getAccelBiasForIMU(imu_index, accel_bias)) {
         bias_z = accel_bias.z;
         return true;
     }
@@ -3749,7 +3749,7 @@ bool AP_AHRS::get_accel_bias_z_for_imu(uint8_t imu_index, float &bias_z) const
 void AP_AHRS::set_inhibit_accel_bias_learning(bool inhibit)
 {
 #if HAL_NAVEKF3_AVAILABLE
-    ekf3.EKF3.setInhibitAccelBiasLearning(inhibit);
+    EKF3.setInhibitAccelBiasLearning(inhibit);
 #endif
 }
 
